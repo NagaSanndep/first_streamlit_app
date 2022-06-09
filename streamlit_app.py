73087e0d99d07095b2b3fcf62737c8f20f5e639a
NagaSanndep <106151440+NagaSanndep@users.noreply.github.com>
2022-06-09 17:11:10 +0530

    Create streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #snowflake-connector-python
-#import Snowflake.Connector
+import Snowflake.Connector
 #snowflake-connector-python
 
 
